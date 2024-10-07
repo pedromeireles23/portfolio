bc1a46430ab3dfcf57c1dc294cf1facf2641745e
pedromeireles23 <pemeirels@gmail.com>
2024-10-07 02:00:05 -0300

    header + header mobile + estilização

---
+++ js-files/mobile-navbar.js
@@ -0,0 +1,54 @@
+class MobileNavbar {
+  constructor() {
+    this.mobileMenu = document.querySelector(".mobile-menu"); 
+    this.navList = document.querySelector(".nav-list-mobile"); 
+    this.navLinks = document.querySelectorAll(".nav-links"); 
+    this.activeClass = "active"; 
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks(){
+    this.navLinks.forEach((link, index)=>{
+      console.log(index/7+0.3);
+      link.style.animation
+      ?(link.style.animation="")
+      :(link.style.animation=`navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`);
+    })
+  }
+  handleClick(){
+    this.navList.classList.toggle(this.activeClass)
+    this.mobileMenu.classList.toggle(this.activeClass)
+    this.animateLinks()
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init(){
+    if(this.mobileMenu){
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list-mobile",
+  ".nav-list-mobile li",
+);
+mobileNavbar.init();
+
+  // const navList = document.querySelector(".nav-list-mobile");
+  
+  // const mobileMenu = document.querySelector(".mobile-menu");
+  // mobileMenu.addEventListener("click", ()=> 
+    
+  //   navList.classList.toggle('open'),
+  //   mobileMenu.classList.toggle('open')
+  
+  
+  
+  // );
