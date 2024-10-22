f43dcd23a1bdd2e1ce1213609287817ae402d8dd
pedromeireles23 <pemeirels@gmail.com>
2024-10-22 00:42:21 -0300

    adição de 2 seções + animações

--- js-files/lightbulb.js
+++ js-files/lightbulb.js
@@ -8,7 +8,6 @@ class LightBulb {
 
     firstClick() {
        
-        this.revealCircle.classList.add('expand');
         this.bodyBulb.style.display = 'none';
         this.otherElements.forEach(element => {
             element.classList.add('appear-elements'); 
@@ -31,47 +30,3 @@ class LightBulb {
 const lightBulb = new LightBulb();
 lightBulb.init();
 
-
-
-// class MobileNavbar  {
-//     constructor() {
-//       this.mobileMenu = document.querySelector(".mobile-menu"); 
-//       this.navList = document.querySelector(".nav-list-mobile"); 
-//       this.navLinks = document.querySelectorAll(".nav-links"); 
-//       this.activeClass = "active"; 
-  
-//       this.handleClick = this.handleClick.bind(this);
-//     }
-  
-//     animateLinks(){
-//       this.navLinks.forEach((link, index)=>{
-//         console.log(index/7+0.3);
-//         link.style.animation
-//         ?(link.style.animation="")
-//         :(link.style.animation=`navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`);
-//       })
-//     }
-//     handleClick(){
-//       this.navList.classList.toggle(this.activeClass)
-//       this.mobileMenu.classList.toggle(this.activeClass)
-//       this.animateLinks()
-//     }
-  
-//     addClickEvent() {
-//       this.mobileMenu.addEventListener("click", this.handleClick);
-//     }
-  
-//     init(){
-//       if(this.mobileMenu){
-//         this.addClickEvent();
-//       }
-//       return this;
-//     }
-//   }
-  
-//   const mobileNavbar = new MobileNavbar(
-//     ".mobile-menu",
-//     ".nav-list-mobile",
-//     ".nav-list-mobile li",
-//   );
-//   mobileNavbar.init();
